9ab0116c882614c55406b7807fe70fc661f69f1e
anujmane007 <anujmane4243@gmail.com>
2023-10-22 20:21:24 +0530

    Gate test 4 Results are declared

--- src/data/resultData.js
+++ src/data/resultData.js
@@ -1,6 +1,7 @@
 import test1 from '../assets/Result/Gate_1_Poster.png'
 import test2 from '../assets/Result/Gate Test 2.png'
 import test3 from '../assets/Result/Gate Test 3.png'
+import test4 from '../assets/Result/Gate Test 4.png'
 import update from '../assets/Past_Events/Update_soon.png'
 // 400 * 284
 export const resultData = [
@@ -30,11 +31,11 @@ export const resultData = [
     },
     {
         id: 4,
-        title: 'Updating Soon..',
-        description: 'Updating Soon..',
-        date: 'Updating Soon..',
-        image: update,
-        url: ''
+        title: 'GATE TEST 4 RESULT ORGANIZED BY CSESA',
+        description: 'Click on this card.....',
+        date: 'Oct 21,2023',
+        image: test4,
+        url: 'https://drive.google.com/file/d/1Aa3Rcj0GIzFkzVyWEcM580dkO-z53tMw/view?usp=sharing'
     },
     {
         id: 5,
